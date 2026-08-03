9ec1781f77cc2da0a6a344607969505ccc9c121f
Aniket-raj82 <aniketrajd8340@gmail.com>
2026-08-03 16:41:54 +0530

    Implemented chat socket

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { createServer } from "node:http";
+import { Server } from "socket.io";
+
 import mongoose from "mongoose";
 import cors from "cors";
 import { connectToSocket } from "./controllers/socketManager.js";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 
@@ -15,6 +18,9 @@ app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+// app.use("/api/v2/users", newUserRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
--- src/controllers/socketManager.js
+++ src/controllers/socketManager.js
@@ -1,7 +1,88 @@
 import { Server } from "socket.io";
 
-export const connectToSocket = (server) => {
-    const io = new Server(server);
-    return io;
-};
+let connections = {}
+let messages = {}
+let timeOnline = {}
 
+export const connectToSocket = (server) => {
+    const io = new Server(server, {
+        cors: {
+            origin : "*",
+            methids: ["GET", "POST"],
+            allowedHeaders: ["*"],
+            credentials: true
+        }
+    });
+
+    io.on("connecction", (socket) => {
+
+        socket.on("join-call", (patch) => {
+            if(conections[path] == undefined){
+                connection[path] = []
+            }
+            connections[path].push(socket.id)
+            timeOnline[socket.id] = new Date();
+            for(let a = 0;a<connection[path].length;i++){
+                io.to(connections[path][a]).emit("user-joined", socket.id,connections[path])
+            }
+            if (message[path] !== undefined) {
+                for (let a = 0; a < message[path].length; a++) {
+                    io.to(socket.id).emit(
+                        "chat-message",
+                        message[path][a].data,
+                        message[path][a].sender,
+                        message[path][a]["socket-id-sender"]
+                    );
+                }
+            }
+        })
+        socket.on("signal", (told,message) => {
+            io.to(toId).emit("signal", socket.id,message);
+        })
+        socket.on("chat-message",(data,sender) => {
+
+            const [matchingRoom,found] = Object.entries(connections)
+            .reduce(([room, isFound], [roomKey, roomValue]) => {
+                if(!isFound && roomValue.includes(socket.id)){
+                    return [roomKey, true];
+                }
+                return [room, isFound];
+            }, ['', false]);
+            if(found == true){
+                if(messages[matchingRoom] == undefined){
+                    messages[matchingRoom] = []
+                }
+                messages[matchingRoom].push({'sender': sender, 'data': data, 'socket-id-sender': socket-id})
+                console.log('message',key,':',sender,data)
+
+                connections[matchingRoom].forEach((elem) => {
+                    io.to(elem).emit("chat-message",data,sender,socket.id)
+                })
+
+            }
+
+        })
+        socket.on("disconnect",()=>{
+            var diffTime = Math.abs(timeOnline[socket.id]-new Date())
+
+            var key 
+            for(const[k,v] of JSON.parse(JSON.stringify(Object.entries(connections)))) {
+                for (let a = 0;a<v.length;++a){
+                    if(v[a] == socket.id){
+                        key = k
+                        for (let a = 0;a<connections[key].length;++a){
+                            io.to(connections[key][a]).emit('user-left',socket.id)
+                        }
+                        var index = connections[key].indexOf(socket.id)
+                        connections[key].splice(index,1)
+                        if(connections[key].length == 0){
+                            delete connections[Key]
+                        }
+                    }
+                }
+            }
+        })
+    })
+
+    return io;
+}
